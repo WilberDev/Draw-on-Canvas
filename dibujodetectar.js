b621ad726c4e8f5eb049a2ab50ad6b8ab3d38e23
WilberDev <wilbecamilo00@gmail.com>
2022-06-07 00:23:48 -0400

    first commit

---
+++ dibujodetectar.js
@@ -0,0 +1,49 @@
+var teclas = {
+  lEFT: 37,
+  UP:38,
+  RIGHT: 39,
+  DOWN:40
+};
+console.log(teclas);
+
+document.addEventListener ("keydown", dibujarTeclado);
+var cuatrito = document.getElementById("area_de_dibujo");
+var papel = cuatrito.getContext("2d");
+var x = 150;
+var y = 150;
+var movimiento = 10;
+dibujarLinea ("red", 149, 149, 151, 151, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+lienzo.beginPath(); //arrancar un dibujo
+lienzo.strokeStyle = color; //linea
+lienzo.lineWidth = 3;
+lienzo.moveTo(xinicial, yinicial);
+lienzo.lineTo(xfinal, yfinal); //traza linea
+lienzo.stroke(); //cerramos linea
+lienzo.closePath(); //cierra dibujo
+}
+function dibujarTeclado(evento)
+{
+  var colorcito = "blue";
+  switch (evento.keyCode)
+   {
+    case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y =  y - movimiento;
+      break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y =  y + movimiento;
+      break;
+    case teclas.lEFT:
+      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+      x =  x - movimiento;
+      break;
+    case teclas.RIGHT:
+      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+      x =  x + movimiento;
+      break;
+    }
+}
